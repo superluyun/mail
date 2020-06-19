96c6ed44e1da09099641a9d5ebddacfb4b72daf5
unknown <1437505934@qq.com>
2020-06-19 20:31:00 +0800

    '6.19'

---
+++ src/permission/permission.js
@@ -0,0 +1,23 @@
+import router from '../router'
+import store from '../store'
+import Storage from '../utils/storage'
+
+const wlist = ["/login"]
+router.beforeEach((to ,from ,next)=>{
+    if(Storage.get('token')){
+        if(to.path==='/login'){
+            next({path:"/"})
+        }else{
+            if(!store.getters.userInfo){
+                store.dispatch("getUserInfo");
+                next();
+            }
+        } 
+    } else {
+        if(wlist.indexOf(to.path) !== -1){
+            next();
+        }else{
+            next(`/login?redirect=${to.path}`)
+        }
+    }
+})
---
+++ src/utils/request.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import axios from 'axios'
+import store from '../store'
+
+Vue.use(axios)
+
+//创建axios实例
+const service = axios.create({
+    baseURL:'/', // api
+    timeout:5000 //请求超时
+})
+
+// request拦截
+service.interceptors.request.use(
+    config =>{
+        if(store.getters.token){
+            config.headers['X-Token'] = store.getters.token
+        }
+        if(config.method === 'post'){
+            config.data = config.params
+            config.params = ''// 清空
+        }
+        return config
+    },
+    error =>{
+        console.err(error)
+        Promise.reject(error)
+    }
+)
+
+// response拦截
+service.interceptors.response.use(
+    response =>{
+        let {data,code,msg} = response.data
+        if(code!=200){
+            // token 过期
+            return Promise.resolve(false);
+        }else{
+            return {data,code,msg}
+        }
+    },
+    error =>{
+        console.err(error)
+        return Promise.reject(error)
+    }
+)
+
+export default service
